c414a3dfba769d3c76c06bec8a801d8586df949e
zz-ederfs <161877224+zz-ederfs@users.noreply.github.com>
2024-08-13 15:15:31 +0000

    main function finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,25 +2,38 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+<i class="fa-solid fa-hourglass-half"></i>;
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = ({ digit1, digit2, digit3, digit4, digit5, digit6 }) => {
+  return (
+    <div className="container-fluid mt-5 pt-5">
+      <div className="row text-center justify-content-center">
+        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
+          <p>
+            <i class="fa-solid fa-hourglass-half pt-3"></i>
+          </p>
+        </div>
+        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
+          <p className="pt-2">{digit6}</p>
+        </div>
+        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
+          <p className="pt-2">{digit5}</p>
+        </div>
+        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
+          <p className="pt-2">{digit4}</p>
+        </div>
+        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
+          <p className="pt-2">{digit3}</p>
+        </div>
+        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
+          <p className="pt-2">{digit2}</p>
+        </div>
+        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
+          <p className="pt-2">{digit1}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,99 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+//// Timer ////
+let sum = 0;
+let digit1 = [0, true];
+let digit2 = [0, true];
+let digit3 = [0, true];
+let digit4 = [0, true];
+let digit5 = [0, true];
+let digit6 = [0, true];
 
+setInterval(() => {
+  if (digit1[0] < 9 && digit1[1] === true) {
+    sum++;
+    digit1[0] = sum;
+  } else if (digit2[0] < 9 && digit2[1] === true) {
+    digit1[1] = false;
+    digit1[0] = 0;
+    sum = digit2[0];
+    sum++;
+    digit2[0] = sum;
+  } else if (digit3[0] < 9 && digit3[1] === true) {
+    digit2[1] = false;
+    digit2[0] = 0;
+    sum = digit3[0];
+    sum++;
+    digit3[0] = sum;
+  } else if (digit4[0] < 9 && digit4[1] === true) {
+    digit3[1] = false;
+    digit3[0] = 0;
+    sum = digit4[0];
+    sum++;
+    digit4[0] = sum;
+  } else if (digit5[0] < 9 && digit5[1] === true) {
+    digit4[1] = false;
+    digit4[0] = 0;
+    sum = digit5[0];
+    sum++;
+    digit5[0] = sum;
+  } else if (digit6[0] < 9 && digit6[1] === true) {
+    digit5[1] = false;
+    digit5[0] = 0;
+    sum = digit6[0];
+    sum++;
+    digit6[0] = sum;
+  }
+  ReactDOM.createRoot(document.getElementById("app")).render(
+    <Home
+      digit1={digit1}
+      digit2={digit2}
+      digit3={digit3}
+      digit4={digit4}
+      digit5={digit5}
+      digit6={digit6}
+    />
+  );
+}, 100);
+
+/* setInterval(() => {
+  digit1++;
+  ReactDOM.createRoot(document.getElementById("app")).render(
+    <Home digit1={digit1} />
+  );
+}, 1000); */
+
+/* function testInterval(params) {
+  if (digit1 < 9) {
+    setInterval(() => {
+      digit1++;
+      ReactDOM.createRoot(document.getElementById("app")).render(
+        <Home digit1={digit1} />
+      );
+    }, 1000);
+  }
+}
+
+testInterval(); */
+/* while (digit6 == 0) {
+  if (digit1 < 9) {
+    setInterval(() => {
+      digit1++;
+      ReactDOM.createRoot(document.getElementById("app")).render(
+        <Home digit1="digit1" digit2="digit2" />
+      );
+    }, 1000);
+  } else if (digit2 < 9) {
+    setInterval(() => {
+      digit1 = 0;
+      digi2++;
+      ReactDOM.createRoot(document.getElementById("app")).render(
+        <Home digit1="digit1" digit2="digit2" />
+      );
+    }, 1000);
+  }
+} */
+
+//render your react application
+//ReactDOM.createRoot(document.getElementById("app")).render(<Home />);
