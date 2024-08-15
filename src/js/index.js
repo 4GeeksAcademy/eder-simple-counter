cebd6429fd9d2637c8a1b5a93faed0a7bdf45802
zz-ederfs <161877224+zz-ederfs@users.noreply.github.com>
2024-08-15 21:36:39 +0000

    minor adjustment

--- src/js/index.js
+++ src/js/index.js
@@ -21,36 +21,54 @@ setInterval(() => {
   if (digit1[0] < 9 && digit1[1] === true) {
     sum++;
     digit1[0] = sum;
+    digit2[1] = true;
   } else if (digit2[0] < 9 && digit2[1] === true) {
     digit1[1] = false;
-    digit1[0] = 0;
+    if (digit6[0] != 9) {
+      digit1[0] = 0;
+    }
     sum = digit2[0];
     sum++;
     digit2[0] = sum;
+    digit3[1] = true;
   } else if (digit3[0] < 9 && digit3[1] === true) {
     digit2[1] = false;
-    digit2[0] = 0;
+    if (digit6[0] != 9) {
+      digit2[0] = 0;
+    }
     sum = digit3[0];
     sum++;
     digit3[0] = sum;
+    digit4[1] = true;
   } else if (digit4[0] < 9 && digit4[1] === true) {
     digit3[1] = false;
-    digit3[0] = 0;
+    if (digit6[0] != 9) {
+      digit3[0] = 0;
+    }
     sum = digit4[0];
     sum++;
     digit4[0] = sum;
+    digit5[1] = true;
   } else if (digit5[0] < 9 && digit5[1] === true) {
     digit4[1] = false;
-    digit4[0] = 0;
+    if (digit6[0] != 9) {
+      digit4[0] = 0;
+    }
     sum = digit5[0];
     sum++;
     digit5[0] = sum;
   } else if (digit6[0] < 9 && digit6[1] === true) {
     digit5[1] = false;
-    digit5[0] = 0;
+    if (digit6[0] != 9) {
+      digit5[0] = 0;
+    }
     sum = digit6[0];
     sum++;
     digit6[0] = sum;
+    if (digit6[0] == 9) {
+      digit1[1] = true;
+      sum = 0;
+    }
   }
   ReactDOM.createRoot(document.getElementById("app")).render(
     <Home
@@ -62,45 +80,4 @@ setInterval(() => {
       digit6={digit6}
     />
   );
-}, 100);
-
-/* setInterval(() => {
-  digit1++;
-  ReactDOM.createRoot(document.getElementById("app")).render(
-    <Home digit1={digit1} />
-  );
-}, 1000); */
-
-/* function testInterval(params) {
-  if (digit1 < 9) {
-    setInterval(() => {
-      digit1++;
-      ReactDOM.createRoot(document.getElementById("app")).render(
-        <Home digit1={digit1} />
-      );
-    }, 1000);
-  }
-}
-
-testInterval(); */
-/* while (digit6 == 0) {
-  if (digit1 < 9) {
-    setInterval(() => {
-      digit1++;
-      ReactDOM.createRoot(document.getElementById("app")).render(
-        <Home digit1="digit1" digit2="digit2" />
-      );
-    }, 1000);
-  } else if (digit2 < 9) {
-    setInterval(() => {
-      digit1 = 0;
-      digi2++;
-      ReactDOM.createRoot(document.getElementById("app")).render(
-        <Home digit1="digit1" digit2="digit2" />
-      );
-    }, 1000);
-  }
-} */
-
-//render your react application
-//ReactDOM.createRoot(document.getElementById("app")).render(<Home />);
+}, 1000);
