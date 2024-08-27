2638dd287793f54f7e983377fd1cf3104e888c13
zz-ederfs <161877224+zz-ederfs@users.noreply.github.com>
2024-08-27 05:19:55 +0000

    reworked basic timer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,35 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 <i class="fa-solid fa-hourglass-half"></i>;
 //create your first component
-const Home = ({ digit1, digit2, digit3, digit4, digit5, digit6 }) => {
+const Home = () => {
+  const [seconds, setSeconds] = useState(0);
+  let newSecond = 0;
+  let control = 0;
+  function updateSeconds() {
+    newSecond++;
+    setSeconds(newSecond);
+  }
+
+  useEffect(() => {
+    const time = setInterval(() => {
+      updateSeconds();
+      control++;
+    }, 1000);
+
+    return () => clearInterval(time);
+  }, [control]);
+
   return (
     <div className="container-fluid mt-5 pt-5">
       <div className="row text-center justify-content-center">
         <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
           <p>
-            <i class="fa-solid fa-hourglass-half pt-3"></i>
+            <i className="fa-solid fa-hourglass-half pt-3"></i>
           </p>
         </div>
-        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
-          <p className="pt-2">{digit6}</p>
-        </div>
-        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
-          <p className="pt-2">{digit5}</p>
-        </div>
-        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
-          <p className="pt-2">{digit4}</p>
-        </div>
-        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
-          <p className="pt-2">{digit3}</p>
-        </div>
-        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
-          <p className="pt-2">{digit2}</p>
-        </div>
-        <div className="col-md-1 display-5 fw-bold text-white bg-dark rounded-pill">
-          <p className="pt-2">{digit1}</p>
+        <div className="col-md-3 display-5 fw-bold text-white bg-dark rounded-pill">
+          <p className="pt-2" style={{ letterSpacing: "20px" }}>
+            {seconds.toString().padStart(6, "0")}
+          </p>
         </div>
       </div>
     </div>
